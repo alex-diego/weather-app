d892f166d2764171ca197bd58450b7a79186de68
Alex Diego <alex.diegosn10@gmail.com>
2021-02-13 16:27:11 -0300

    weather app

---
+++ README.md
@@ -0,0 +1,9 @@
+# Weather App JS 🌞
+
+## Sobre: 
+Uma simples aplicação web para saber o clima atual da cidade pesquisada, usando uma <a href="https://openweathermap.org/api" target="_blank">Weather API</a>. Basta escrever o nome da cidade e em seguida clicar em "Buscar", a aplicação mostrará a temperatura atual, máxima e mínima do dia.
+
+
+<img src="./img/Screenshot_1.png"/>
+<img src="./img/Screenshot_2.png"/>
+<img src="./img/gif.gif"/>
---
+++ js/script.js
@@ -0,0 +1,61 @@
+const App = {
+  search() {
+    const city = DOM.input.value;
+    const key = '55ad43d6f008f8766ad4b6c58faa27d9';
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`;
+
+    if (city === "") {
+      alert("Por Favor digite o nome de uma cidade!");
+      return;
+    }
+
+    fetch(api)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        DOM.renderLayout(data);
+      })
+      .catch((error) => {
+        alert("Ocorreu algum erro :(")
+        console.log(error)
+      })
+  }
+}
+
+const DOM = {
+  input: document.querySelector('input.search-city'),
+  container: document.querySelector('.container'),
+
+  toggleContainer() {
+    DOM.container.classList.add('initial');
+  },
+
+  renderLayout(data) {
+    const layout = `
+    <div class="info-container">
+      <div class="main-info">
+        <h1 class="city">${data.name}</h1>
+        <h2 class="degrees">${Number(data.main.temp).toFixed(0)}<span>Cº</span></h2>
+      </div>
+      <div class="icon-container">
+        <img src="./img/icons/${data.weather[0].icon}.png" alt="${data.weather[0].description}" class="icon">
+      </div>
+    </div>
+    <div class="min-max-container">
+      <span class="min-max">MIN - MAX</span>
+      <div class="info-bar">
+        <span>${data.main.temp_min}Cº</span>
+        <div class="bar-container">
+          <div class="blue-bar"></div>
+          <div class="red-bar"></div>
+        </div>
+        <span>${data.main.temp_max}Cº</span>
+      </div>
+    </div>
+    `
+
+    DOM.container.innerHTML = layout;
+    DOM.toggleContainer();
+  }
+}
